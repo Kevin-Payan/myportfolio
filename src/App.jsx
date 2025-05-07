70bf28dab0cc78cbf0235e2e657f292e90496fbf
Kevin Payan <125767404+Kevin-Payan@users.noreply.github.com>
2025-05-06 21:52:25 -0700

    started hero section

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Hero/>
-      <About/>
-      <Contact/>
+      {/* <About/>
+      <Contact/> */}
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { LuGithub } from "react-icons/lu";
+import { LiaLinkedin } from "react-icons/lia";
+import { LuMail } from "react-icons/lu";
+
 
 const Hero = () => {
   return (
-    <div>Hero Section</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl'>Hi, I'm Kevin Payan</h1>
+      <h2 className='text-2xl'>I'm a Software Engineer</h2>
+      <button className='bg-custom-border px-4 py-2 rounded m-3'>
+        Placeholder
+      </button>
+      <div className='flex items-center justify-center space-x-4 m-2'>
+        <a href="https://github.com/Kevin-Payan"><LuGithub className='w-6 h-6' /></a>
+        <a href="https://www.linkedin.com/in/kevinpayan/"><LiaLinkedin className='w-8 h-8' /></a>
+        <a href="mailto:kevinpn@hotmail.com"><LuMail className='w-6 h-6' /></a>
+      </div>
+    </div>
   )
 }
 
