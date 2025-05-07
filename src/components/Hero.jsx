6fbb8f0714cc725147b3cdd1e2b4a8da4a42c5ce
Kevin Payan <125767404+Kevin-Payan@users.noreply.github.com>
2025-05-07 12:19:11 -0700

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,19 @@ import { LuMail } from "react-icons/lu";
 const Hero = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-4xl'>Hi, I'm Kevin Payan</h1>
-      <h2 className='text-2xl'>I'm a Software Engineer</h2>
-      <button className='bg-custom-border px-4 py-2 rounded m-3'>
-        Placeholder
+      <div className='relative'>
+        <img src="/src/assets/me.png" alt="image of me" className='top-0 z-0' />
+        <div class="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-custom-background to-transparent pointer-events-none"></div>
+      </div>
+      <div className='flex flex-col justify-center items-center z-10'>
+      <h1 className='text-4xl'>Hi, I'm</h1>
+      <h1 className='text-4xl'>Kevin Payan</h1>
+      <h2 className='text-2xl m-2'>I'm a Software Engineer</h2>
+      </div>
+      <button className='bg-custom-border px-4 py-2 rounded m-3 z-10 text-custom-accent'>
+        View my work
       </button>
-      <div className='flex items-center justify-center space-x-4 m-2'>
+      <div className='flex items-center justify-center space-x-4 m-2 z-10'>
         <a href="https://github.com/Kevin-Payan"><LuGithub className='w-6 h-6' /></a>
         <a href="https://www.linkedin.com/in/kevinpayan/"><LiaLinkedin className='w-8 h-8' /></a>
         <a href="mailto:kevinpn@hotmail.com"><LuMail className='w-6 h-6' /></a>
